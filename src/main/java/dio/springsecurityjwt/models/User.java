9e89070938fc24f20845851e9f18deaa44ca70e8
Anthony Cruz <tony_esqueloto@hotmail.com>
2023-12-18 17:41:43 -0300

    feat: create User model

---
+++ src/main/java/dio/springsecurityjwt/models/User.java
@@ -0,0 +1,32 @@
+package dio.springsecurityjwt.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user")
+    private Integer id;
+    @Column(length = 50, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false, unique = true)
+    private String username;
+    @Column(length = 100, nullable = false)
+    private String password;
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tb_user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role_id")
+    private List<String> roles = new ArrayList<>();
+}
